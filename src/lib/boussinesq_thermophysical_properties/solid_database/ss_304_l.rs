2b827d82e6b71e44ccf18178118b6254e6ef0315
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-10-28 17:23:25 +0800

    cleaned up steel ss304l enthalpy and temperature from enthalpy

--- src/lib/boussinesq_thermophysical_properties/solid_database/ss_304_l.rs
+++ src/lib/boussinesq_thermophysical_properties/solid_database/ss_304_l.rs
@@ -1,7 +1,10 @@
+use roots::{find_root_brent, SimpleConvergency};
+use specific_enthalpy::try_get_h;
 use uom::si::available_energy::{joule_per_gram, joule_per_kilogram};
 use uom::si::f64::*;
 use uom::si::length::millimeter;
 use uom::si::mass_density::kilogram_per_cubic_meter;
+use uom::si::pressure::atmosphere;
 use uom::si::specific_heat_capacity::joule_per_kilogram_kelvin;
 use uom::si::thermal_conductivity::watt_per_meter_kelvin;
 use crate::boussinesq_thermophysical_properties::*;
@@ -514,6 +517,206 @@ pub fn specific_enthalpy_test_steel_ornl_and_zweibaum_spline(){
         enthalpy_analytical_ornl.value,
         enthalpy_spline_zweibaum.value,
         max_relative=0.045);
+}
+/// returns temperature of stainless steel 304L 
+/// cited from:
+/// Zou, L., Hu, R., & Charpentier, A. (2019). SAM code 
+/// validation using the compact integral effects test (CIET) experimental 
+/// data (No. ANL/NSE-19/11). Argonne National 
+/// Lab.(ANL), Argonne, IL (United States).
+///
+/// The algorithm is to make a spline of enthalpy and temperature
+/// and then use the enthalpy to obtain a temperature
+///
+/// Note: h in this function represents specific enthalpy
+///
+/// This uses the spline methodology as an initial guess
+/// but then uses brent dekker to finish it off
+#[inline]
+pub (crate)fn steel_304_l_spline_temp_attempt_3_from_specific_enthalpy_ciet_zweibaum(
+    h_steel: AvailableEnergy) -> ThermodynamicTemperature {
+
+    // the idea is basically to evaluate enthalpy at the 
+    // following temperatures
+    let temperature_values_kelvin: Vec<f64>
+    = c!(250.0, 300.0, 350.0, 
+        400.0, 450.0, 500.0, 700.0, 1000.0);
+
+    // and then use that to formulate a spline,
+    // with the spline, i'll evaluate enthalpy from temperature
+    // within pretty much one iteration. However, it is spline 
+    // construction which may take a little long. 
+    //
+    // However, the number of iterations per calculation is fixed
+    //
+    // I won't optimise it now just yet
+
+    let temperature_vec_len = 
+    temperature_values_kelvin.len();
+
+    let mut enthalpy_vector = vec![0.0; temperature_vec_len];
+
+    for index_i in 0..temperature_vec_len {
+
+        // first, evaluate the enthalpy at temperature values 
+        let temperature_value = temperature_values_kelvin[index_i];
+
+        //next let's evaluate the specific enthalpy of steel 
+        let steel = Material::Solid(SolidMaterial::SteelSS304L);
+        let steel_temp = ThermodynamicTemperature::new::<kelvin>(
+            temperature_value);
+        let pressure = Pressure::new::<atmosphere>(1.0);
+
+        let steel_enthalpy_result = try_get_h(steel, 
+            steel_temp, pressure);
+
+        let steel_enthalpy_value = match steel_enthalpy_result {
+            Ok(steel_enthalpy) => steel_enthalpy.value,
+            // i can of course unwrap the result,
+            // but i want to leave it more explicit in case 
+            // i wish to manually handle the error
+            Err(error_msg) => panic!("{}",error_msg),
+        };
+
+        // once i evalute the enthalpy value, pass it on to the vector
+
+        enthalpy_vector[index_i] = steel_enthalpy_value;
+
+    }
+
+
+    // now I have my enthalpy vector, i can do an inverted spline 
+    // to have enthalpy given in as an input, and temperature received
+    // as an output
+
+    let enthalpy_to_temperature_spline = 
+    CubicSpline::from_nodes(&enthalpy_vector,
+    &temperature_values_kelvin);
+
+    // now let's get our enthalpy in joules_per_kg
+    let h_steel_joules_per_kg = h_steel.get::<joule_per_kilogram>();
+
+    let temperature_from_enthalpy_kelvin = 
+    enthalpy_to_temperature_spline.unwrap().eval(h_steel_joules_per_kg);
+
+    let enthalpy_root = |temp_degrees_c_value : f64| -> f64 {
+        let lhs_value = h_steel.get::<joule_per_kilogram>();
+
+
+        let steel = Material::Solid(SolidMaterial::SteelSS304L);
+        let steel_temp = ThermodynamicTemperature::new::
+            <kelvin>(temp_degrees_c_value) ;
+        let pressure = Pressure::new::<atmosphere>(1.0);
+
+        let rhs = try_get_h(steel, 
+            steel_temp, pressure);
+
+        let rhs_value = match rhs {
+            Ok(enthalpy_val) => enthalpy_val.get::<joule_per_kilogram>(),
+                // fall back to guess value, 
+            Err(error_msg) => panic!("{}",error_msg),
+        };
+
+        return lhs_value-rhs_value;
+    };
+
+    // brent error bounds can be smaller for this steel spline, 
+    // it's a lot more accurate than for steel
+    let brent_error_bound: f64 = 1.0;
+
+    let upper_limit: f64 = temperature_from_enthalpy_kelvin +
+        brent_error_bound;
+
+    let lower_limit : f64 = temperature_from_enthalpy_kelvin -
+        brent_error_bound;
+
+    let mut convergency = SimpleConvergency { eps:1e-8f64, max_iter:30 };
+    let fluid_temperature_degrees_c_result
+    = find_root_brent(upper_limit,
+        lower_limit,
+        enthalpy_root,
+        &mut convergency
+    );
+
+    // we can extract the temperature 
+    // but if this doesn't work, bracket the entire range
+    // 250K to 1000K 
+
+    //let upper_limit_full_range: f64 = 1000.0;
+    //let lower_limit_full_range: f64 = 250.0;
+    //
+    // or better yet, just panic the value for steel enthalpy
+
+    let temperature_from_enthalpy_kelvin: f64 = 
+    match fluid_temperature_degrees_c_result {
+        Ok(temperature_val) => temperature_val,
+        Err(_) => panic!("{:?}",h_steel),
+            //find_root_brent(upper_limit_full_range, 
+            //lower_limit_full_range, 
+            //enthalpy_root, 
+            //&mut convergency).unwrap(),
+
+    };
+
+    // return temperature
+    ThermodynamicTemperature::new::<kelvin>(
+        temperature_from_enthalpy_kelvin)
+
+}
+
+/// this is my third, and final iteration of getting enthalpy as a function 
+/// of temperature
+#[test]
+pub fn steel_temperature_from_enthalpy_test_spline_3(){
+    // we'll test temperature at 375K 
+    // we should get an enthalpy from the spline 
+    // for zweibaum's paper 
+
+    let steel = Material::Solid(SteelSS304L);
+    let steel_temp = ThermodynamicTemperature::new::<kelvin>(375.0);
+    let atmospheric_pressure = Pressure::new::<atmosphere>(1.0);
+
+    let enthalpy_spline_zweibaum_375k = try_get_h(
+        steel,steel_temp,atmospheric_pressure).unwrap();
+
+    // now we have an enthalpy, let's check the temperature 
+
+    let temperature_from_enthalpy_test = 
+    steel_304_l_spline_temp_attempt_3_from_specific_enthalpy_ciet_zweibaum(
+        enthalpy_spline_zweibaum_375k);
+
+    // we are basically off by less than 0.05K, which is 
+    // within measurement error!
+    approx::assert_abs_diff_eq!(
+    temperature_from_enthalpy_test.get::<kelvin>(),
+    375.0,
+    epsilon=0.00005);
+
+    // let's test from 325, 425, 525, 625, 725, 825, 925
+    let temperature_vec_kelvin: Vec<f64> = 
+    vec![325.0, 425.0, 525.0, 625.0, 725.0, 825.0, 925.0];
+
+    for temperature_val_kelvin in temperature_vec_kelvin.iter() {
+
+        let steel_temp = ThermodynamicTemperature::new::<kelvin>(
+            *temperature_val_kelvin);
+
+        let enthalpy_spline_zweibaum = try_get_h(steel, 
+            steel_temp, atmospheric_pressure).unwrap();
+
+        let temperature_from_enthalpy_test = 
+        steel_304_l_spline_temp_attempt_3_from_specific_enthalpy_ciet_zweibaum(
+            enthalpy_spline_zweibaum);
+
+        // we are basically off by less than 0.5K, which is 
+        // within measurement error!
+        approx::assert_abs_diff_eq!(
+        temperature_from_enthalpy_test.get::<kelvin>(),
+        *temperature_val_kelvin,
+        epsilon=0.00005);
+    };
+
+
 }
 
 #[inline]
