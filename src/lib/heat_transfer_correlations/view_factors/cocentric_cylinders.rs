1464df47f3afe32686bbfe41427352031688b7e5
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-11-06 14:13:16 +0800

    cocentric cylinder self view factor coded in

--- src/lib/heat_transfer_correlations/view_factors/cocentric_cylinders.rs
+++ src/lib/heat_transfer_correlations/view_factors/cocentric_cylinders.rs
@@ -1,5 +1,7 @@
+use std::f64::consts::PI;
+
 use ndarray_linalg::Scalar;
-use uom::si::f64::*;
+use uom::si::{f64::*, ratio::ratio};
 
 /// F_(2-2) =  1 - 1/R - ((H^2 + 4 R^2)^0.5 -H)/(4 R) + 1/PI * B
 ///
@@ -42,9 +44,17 @@ pub fn outer_cylinder_self_view_factor(
 
     let c = d * e.asin() - f.asin();
 
+    let mut b = 2.0 * one_over_r * (2.0 * (r_sq-1.0).sqrt() * one_over_h).atan();
+    b -= h_value * 0.5 * one_over_r * c;
+
+    let view_factor_value: f64 = 
+        1.0 - one_over_r
+        - 0.25 * one_over_r * ((h_sq+ 4.0 * r_sq).sqrt() - h_value)
+        + PI.recip() * b;
 
 
-    todo!()
+
+    return Ratio::new::<ratio>(view_factor_value);
 
 
 }
