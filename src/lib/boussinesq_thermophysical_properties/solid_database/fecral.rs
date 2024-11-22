e085aa7fb4aeb41b49dc908e61617e2134014f70
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-11-22 11:40:20 +0800

    added fecral constant cp estaimte

--- src/lib/boussinesq_thermophysical_properties/solid_database/fecral.rs
+++ src/lib/boussinesq_thermophysical_properties/solid_database/fecral.rs
@@ -14,12 +14,32 @@ use crate::boussinesq_thermophysical_properties::*;
 use crate::tuas_lib_error::TuasLibError;
 use uom::si::thermodynamic_temperature::kelvin;
 
-// Wang, X., Lu, Z., Li, Z., Shi, Y., & Xu, H. (2022). 
-// Effect of Zr content on microstructure and hardness of ODS-FeCrAl 
-// alloys. Materials Characterization, 192, 112221.
-//
-// based on chromium steel, 7.8 g/cm3
-//
+/// Wang, X., Lu, Z., Li, Z., Shi, Y., & Xu, H. (2022). 
+/// Effect of Zr content on microstructure and hardness of ODS-FeCrAl 
+/// alloys. Materials Characterization, 192, 112221.
+///
+/// based on chromium steel, 7.8 g/cm3
+///
 pub fn fecral_constant_density() -> Result<MassDensity,TuasLibError> {
     return Ok(MassDensity::new::<gram_per_cubic_centimeter>(7.8));
 }
+
+pub fn fecral_surf_roughness() -> Length {
+    todo!()
+}
+
+
+/// Field, K. G., Snead, M. A., Yamamoto, Y., & Terrani, K. A. (2017). 
+/// Handbook on the material properties of FeCrAl alloys for nuclear 
+/// power production applications. Nuclear Technology Research and 
+/// Development.
+///
+/// While cp is temperature dependent, this provides a rough 
+/// estimate in for simpler approximate calculations 
+///
+/// However, it is reasonably accurate in the range 700-1000C for  
+/// fecral undergoing cooling. There is hysteresis apparently
+pub fn fecral_const_cp_estimate() -> Result<SpecificHeatCapacity, TuasLibError> {
+    return Ok(SpecificHeatCapacity::new::<joule_per_kilogram_kelvin>(0.8));
+}
+
