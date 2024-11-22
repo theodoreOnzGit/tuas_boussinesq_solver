0709a26be4b72d8052799d637b503a9a8e88dbc1
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-11-22 11:51:14 +0800

    added fecral thermal condcutivity

--- src/lib/boussinesq_thermophysical_properties/solid_database/fecral.rs
+++ src/lib/boussinesq_thermophysical_properties/solid_database/fecral.rs
@@ -1,4 +1,5 @@
 use uom::si::diffusion_coefficient::square_millimeter_per_second;
+use peroxide::fuga::{CubicSpline, Spline};
 use uom::si::f64::*;
 use roots::{find_root_brent, SimpleConvergency};
 use specific_enthalpy::try_get_h;
@@ -10,7 +11,7 @@ use uom::si::pressure::atmosphere;
 use uom::si::specific_heat_capacity::joule_per_kilogram_kelvin;
 use uom::si::specific_power::kilowatt_per_kilogram;
 use uom::si::thermodynamic_temperature::degree_celsius;
-use uom::si::thermal_conductivity::milliwatt_per_meter_kelvin;
+use uom::si::thermal_conductivity::{milliwatt_per_meter_kelvin, watt_per_meter_kelvin};
 use crate::boussinesq_thermophysical_properties::*;
 use crate::tuas_lib_error::TuasLibError;
 use uom::si::thermodynamic_temperature::kelvin;
@@ -40,6 +41,7 @@ pub fn fecral_surf_roughness() -> Length {
 ///
 /// However, it is reasonably accurate in the range 700-1000C for  
 /// fecral undergoing cooling. There is hysteresis apparently
+/// based on fig 15
 pub fn fecral_const_cp_estimate() -> Result<SpecificHeatCapacity, TuasLibError> {
     return Ok(SpecificHeatCapacity::new::<joule_per_kilogram_kelvin>(0.8));
 }
@@ -54,6 +56,8 @@ pub fn fecral_const_cp_estimate() -> Result<SpecificHeatCapacity, TuasLibError> 
 /// While thermal diffusivity is temperature dependent, this provides a rough 
 /// estimate in for simpler approximate calculations 
 ///
+/// based on fig 15
+///
 pub fn fecral_const_thermal_diffusivity_estimate() -> 
 Result<DiffusionCoefficient, TuasLibError> {
     return Ok(DiffusionCoefficient::new::<square_millimeter_per_second>(4.0));
@@ -65,8 +69,28 @@ Result<DiffusionCoefficient, TuasLibError> {
 /// power production applications. Nuclear Technology Research and 
 /// Development.
 ///
-/// 
+/// approximately based on fig 5
+///
+/// linear relationship assumed
+///
+/// roughly based on the average between APMT, C06M, C35M and C36M fecral
 pub fn fecral_thermal_conductivity(temperature: ThermodynamicTemperature) -> 
 Result<ThermalConductivity,TuasLibError> {
-    todo!()
+    //range_check(
+    //    &Material::Solid(SolidMaterial::PyrogelHPS),
+    //    temperature, 
+    //    ThermodynamicTemperature::new::<degree_celsius>(39.819), 
+    //    ThermodynamicTemperature::new::<degree_celsius>(9.88))?;
+    let fecral_generic_kappa_temperature_values_kelvin = c!(
+        300.0,1400.0);
+    let fecral_generic_kappa_values_watts_per_meter_kelvin = c!(
+        12.0,25.0);
+    let s = CubicSpline::from_nodes(&fecral_generic_kappa_temperature_values_kelvin, 
+        &fecral_generic_kappa_values_watts_per_meter_kelvin);
+    let temperature_value_degc: f64 = temperature.get::<kelvin>();
+    let fecral_kappa_watt_per_meter_kelvin = 
+        s.unwrap().eval(temperature_value_degc);
+
+    return Ok(ThermalConductivity::new::<watt_per_meter_kelvin>(
+            fecral_kappa_watt_per_meter_kelvin));
 }
