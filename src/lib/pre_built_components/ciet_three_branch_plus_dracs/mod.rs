7a11597181c02de33e6b23480af3bd39564dfb1e
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-11-29 08:29:59 +0800

    adding ciet educational simulation

---
+++ src/lib/pre_built_components/ciet_three_branch_plus_dracs/mod.rs
@@ -0,0 +1,29 @@
+/// this version of ciet is optimised for real-time 
+/// simulation. It will not be validated, but it will be 
+/// fun to play with as a simulator. Useful for education and etc.
+///
+/// 
+pub mod ciet_educational_simulator_loop;
+
+/// CIET needs Thermo-hydraulic equations solved in TUAS 
+///
+/// Writing them out explicitly in a procedural form is quite 
+/// cumbersome. It is much more concise to have these functions 
+/// here
+///
+/// Here there are functions for connecting the 
+/// heat transfer entities in:
+///
+/// - dracs loop 
+/// - pri loop DHX branch
+/// - pri loop heater branch 
+/// - pri loop CTAH branch (forced circ)
+///
+/// Also I need to solve fluid flow in 
+/// - dracs loop 
+/// - pri loop, DHX, heater and CTAH branch
+///
+/// I would also need to be able to block flow in pri loop DHX 
+/// and CTAH branch as well, so as to isolate the loops.
+///
+pub mod solver_functions;
