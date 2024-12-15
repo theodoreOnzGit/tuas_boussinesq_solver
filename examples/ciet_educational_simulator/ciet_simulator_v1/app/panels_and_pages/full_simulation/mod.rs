95c6a0297600d676eccfb8c8576d8eb6f03e58a1
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-12-15 17:14:45 +0800

    minimum ctah heat transfer now 1 w/m2 k

--- examples/ciet_educational_simulator/ciet_simulator_v1/app/panels_and_pages/full_simulation/mod.rs
+++ examples/ciet_educational_simulator/ciet_simulator_v1/app/panels_and_pages/full_simulation/mod.rs
@@ -597,7 +597,7 @@ pub fn educational_ciet_loop_version_3(
             // make sure it cannot be less than a certain amount 
             let ctah_minimum_heat_transfer = 
                 HeatTransfer::new::<watt_per_square_meter_kelvin>(
-                    5.0);
+                    1.0);
 
             // this makes it physically realistic
             if ctah_heat_trf_output < ctah_minimum_heat_transfer {
