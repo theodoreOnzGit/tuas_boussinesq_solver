c4425e8b1f675eb25cf86e71fbf2df93d37b1d84
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-12-15 15:14:45 +0800

    adjusted integral controller to be less aggressive

--- examples/ciet_educational_simulator/ciet_simulator_v1/app/panels_and_pages/full_simulation/mod.rs
+++ examples/ciet_educational_simulator/ciet_simulator_v1/app/panels_and_pages/full_simulation/mod.rs
@@ -57,7 +57,7 @@ pub fn educational_ciet_loop_version_3(
     // controller for tchx 
 
     let tchx_controller_gain = Ratio::new::<ratio>(9.75);
-    let tchx_integral_time: Time = tchx_controller_gain / Frequency::new::<hertz>(5.0);
+    let tchx_integral_time: Time = tchx_controller_gain / Frequency::new::<hertz>(1.0);
     let tchx_derivative_time: Time = Time::new::<second>(1.0);
     // derivative time ratio
     let tchx_alpha: Ratio = Ratio::new::<ratio>(1.0);
@@ -76,7 +76,7 @@ pub fn educational_ciet_loop_version_3(
 
     // controller for ctah
     let ctah_controller_gain = Ratio::new::<ratio>(9.75);
-    let ctah_integral_time: Time = ctah_controller_gain / Frequency::new::<hertz>(5.0);
+    let ctah_integral_time: Time = ctah_controller_gain / Frequency::new::<hertz>(1.0);
     let ctah_derivative_time: Time = Time::new::<second>(1.0);
     // derivative time ratio
     let ctah_alpha: Ratio = Ratio::new::<ratio>(1.0);
