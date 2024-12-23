eb6bab8788032eea96817ac506be435eebbb7977
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-12-23 10:00:50 +0800

    added heated section regression test, asserted that old and new outlet temperatures are same to within 1% at every timestep

--- src/lib/pre_built_components/non_insulated_porous_media_fluid_components/tests/mod.rs
+++ src/lib/pre_built_components/non_insulated_porous_media_fluid_components/tests/mod.rs
@@ -7,6 +7,16 @@ mod heater_example_with_data_record;
 #[cfg(test)]
 mod heated_section_only;
 
+/// basically, I updated the heater v2 class to be a more generic 
+/// porous media pipe without insulation. I just want to make sure 
+/// this is working properly in terms of lateral connections and 
+/// advancing timesteps etc. 
+///
+/// the old heater code and new heater code should perform the same 
+/// way
+#[cfg(test)]
+mod heated_section_regression;
+
 /// this does not have csv writer, was used for early test and development
 #[cfg(test)]
 mod heated_section_with_top_bottom_heads;
