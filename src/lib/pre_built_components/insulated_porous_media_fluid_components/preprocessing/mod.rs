c2cfcdf48a2266b03779b2d8c152a4d02d38d9c2
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-12-25 13:09:44 +0800

    added ambient to insulation conductance, needs verification

--- src/lib/pre_built_components/insulated_porous_media_fluid_components/preprocessing/mod.rs
+++ src/lib/pre_built_components/insulated_porous_media_fluid_components/preprocessing/mod.rs
@@ -19,6 +19,7 @@ use crate::boussinesq_thermophysical_properties::SolidMaterial;
 use crate::boundary_conditions::BCType;
 use crate::array_control_vol_and_fluid_component_collections::one_d_solid_array_with_lateral_coupling::SolidColumn;
 use crate::array_control_vol_and_fluid_component_collections::one_d_fluid_array_with_lateral_coupling::FluidArray;
+use crate::tuas_lib_error::TuasLibError;
 
 impl InsulatedPorousMediaFluidComponent {
 
@@ -64,6 +65,71 @@ impl InsulatedPorousMediaFluidComponent {
 
         self.pipe_shell.link_to_back(&mut zero_power_bc,
             interaction).unwrap();
+    }
+
+    /// obtains the conductance from ambient to the pipe shell 
+    /// nodally speaking 
+
+    #[inline]
+    pub fn get_ambient_to_insulation_nodal_conductance(&mut self,
+        heat_transfer_to_ambient: HeatTransfer) 
+        -> Result<ThermalConductance,TuasLibError> {
+
+            // the solid conductance is calculated using 
+            // k (A/L) where L is representative thickness 
+            // A is heat transfer area,
+            // k is thermal conductivity
+            //
+            let insulation_conductance_lengthscale: Length = 
+                self.thermal_conductance_lengthscale_insulation_to_ambient;
+            
+            // to calculate k, we need the bulk temperature 
+
+            let mut insulation_clone: SolidColumn = 
+                self.insulation_array.clone().try_into().unwrap();
+            let insulation_bulk_temp: ThermodynamicTemperature = 
+                insulation_clone.try_get_bulk_temperature()?;
+
+            // next, let's get the conductivity 
+
+            let insulation_material_conductivity: ThermalConductivity = 
+                insulation_clone.material_control_volume
+                .try_get_thermal_conductivity(
+                    insulation_bulk_temp)?;
+
+            let number_of_nodes: f64 = self.inner_nodes as f64 + 2.0;
+            // solid side nodalised thermal conductance
+
+            let nodalised_insulation_side_thermal_conductance: ThermalConductance
+                = insulation_conductance_lengthscale * insulation_material_conductivity
+                / number_of_nodes;
+
+            let nodalised_insulation_side_thermal_resistance: ThermalResistance 
+                = nodalised_insulation_side_thermal_conductance.recip();
+
+            // next, nodalised thermal conductance due to liquid side 
+            
+            let nodalised_thermal_conductance_ambient_convection: ThermalConductance 
+                = (heat_transfer_to_ambient * self.convection_heat_transfer_area_insulation_to_ambient)
+                / number_of_nodes;
+
+            let nodalised_ambient_convection_thermal_resistance: ThermalResistance 
+                = nodalised_thermal_conductance_ambient_convection.recip();
+
+
+            // add resistances together 
+            let nodalised_insulation_to_ambient_resistance: ThermalResistance 
+                = nodalised_insulation_side_thermal_resistance +
+                nodalised_ambient_convection_thermal_resistance;
+
+            // get conductance, and then return 
+
+            let nodalised_insulation_to_ambient_conductance 
+                = nodalised_insulation_to_ambient_resistance.recip();
+
+            // and we done!
+            return Ok(nodalised_insulation_to_ambient_conductance);
+
     }
 }
 
