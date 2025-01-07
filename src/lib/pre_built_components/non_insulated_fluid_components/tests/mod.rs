0f31be39533a92921ec89036a5785ce3347f53f6
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2025-01-07 21:34:47 +0800

    added heated section validation tests for non insulated fluid components

---
+++ src/lib/pre_built_components/non_insulated_fluid_components/tests/mod.rs
@@ -0,0 +1,10 @@
+
+/// this does not have csv writer, was used for early test and development
+///
+/// but now also becomes a unit test for validation at steady state using 
+/// De Wet's data on heater v2 (page 64 of PhD thesis)
+/// Ong, T. K. C. [2024a]. Digital Twins as Testbeds 
+/// for Iterative Simulated Neutronics Feedback Controller 
+/// Development. UC Berkeley.
+#[cfg(test)]
+mod heated_section_only;
