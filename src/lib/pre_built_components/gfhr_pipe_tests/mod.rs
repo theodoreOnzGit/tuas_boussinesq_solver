47bf01c5d06b2913c010130361e37b03d1ffe4a1
Theodore Ong at lenovo thinkpad windows E14 <theodore_ong@berkeley.edu>
2025-06-22 12:04:45 +0800

    organised gfhr pipe tests to components

--- src/lib/pre_built_components/gfhr_pipe_tests/mod.rs
+++ src/lib/pre_built_components/gfhr_pipe_tests/mod.rs
@@ -1,6 +1,10 @@
+pub mod components;
 /// first, single pipe tests 
 ///
 /// this is where single pipes of the typical fhrs are concerned 
 /// both pressure change from mass flowrate and vice verse should be tested
 /// in fwd and backwd flow
 pub mod single_pipe_tests;
+
+
+
