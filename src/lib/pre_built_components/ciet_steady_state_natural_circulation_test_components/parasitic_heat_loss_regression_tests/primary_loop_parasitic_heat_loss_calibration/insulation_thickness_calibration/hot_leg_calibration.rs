2f71eb168074f4460bb5cfc8cc3964f4367cbe43
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2025-07-02 11:19:59 +0800

    added doc comments for hot leg validation of ciet coupled nat circ loop

--- src/lib/pre_built_components/ciet_steady_state_natural_circulation_test_components/parasitic_heat_loss_regression_tests/primary_loop_parasitic_heat_loss_calibration/insulation_thickness_calibration/hot_leg_calibration.rs
+++ src/lib/pre_built_components/ciet_steady_state_natural_circulation_test_components/parasitic_heat_loss_regression_tests/primary_loop_parasitic_heat_loss_calibration/insulation_thickness_calibration/hot_leg_calibration.rs
@@ -141,8 +141,8 @@ pub fn hot_leg_insulation_thickness_calibration_v1(){
 
 
 }
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-1,0.02003,75.22747,71.47752,53.60943,50.45784,
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-1,0.02003,75.22747,71.47752,53.60943,50.45784,
 #[test]
 pub fn hot_leg_validation_set_c1(){
 
@@ -167,8 +167,8 @@ pub fn hot_leg_validation_set_c1(){
 
 
 }
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-2,0.02367,82.41863,78.36713,57.13467,53.79036,
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-2,0.02367,82.41863,78.36713,57.13467,53.79036,
 #[test]
 pub fn hot_leg_validation_set_c2(){
 
@@ -193,8 +193,8 @@ pub fn hot_leg_validation_set_c2(){
 
 
 }
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-3,0.02635,87.78188,84.37342,59.82845,56.71891,
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-3,0.02635,87.78188,84.37342,59.82845,56.71891,
 #[test]
 pub fn hot_leg_validation_set_c3(){
 
@@ -219,8 +219,8 @@ pub fn hot_leg_validation_set_c3(){
 
 
 }
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-4,0.02949,94.71628,90.97595,63.9812,60.83029,
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-4,0.02949,94.71628,90.97595,63.9812,60.83029,
 #[test]
 pub fn hot_leg_validation_set_c4(){
 
@@ -245,10 +245,10 @@ pub fn hot_leg_validation_set_c4(){
 
 
 }
-// Based on Set c1 to c4 the average insulation is about 0.2495 cm
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-5,0.0319,100.37023,96.20228,67.05336,64.07406,
-//
+/// Based on Set c1 to c4 the average insulation is about 0.2495 cm
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-5,0.0319,100.37023,96.20228,67.05336,64.07406,
+///
 #[test]
 pub fn hot_leg_validation_set_c5(){
 
@@ -275,11 +275,11 @@ pub fn hot_leg_validation_set_c5(){
 }
 
 
-// Based on Set c1 to c4 the average insulation is about 0.2495 cm
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-6,0.03412,105.25073,101.3375,69.85085,67.1654,
-//
-//
+/// Based on Set c1 to c4 the average insulation is about 0.2495 cm
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-6,0.03412,105.25073,101.3375,69.85085,67.1654,
+///
+///
 #[test]
 pub fn hot_leg_validation_set_c6(){
 
@@ -305,9 +305,9 @@ pub fn hot_leg_validation_set_c6(){
 
 }
 
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-7,0.03562,110.34289,106.43149,73.21226,70.6215,
-//
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-7,0.03562,110.34289,106.43149,73.21226,70.6215,
+///
 #[test]
 pub fn hot_leg_validation_set_c7(){
 
@@ -333,9 +333,9 @@ pub fn hot_leg_validation_set_c7(){
 
 }
 
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-8,0.03593,115.52364,111.37615,76.13202,73.63344,
-//
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-8,0.03593,115.52364,111.37615,76.13202,73.63344,
+///
 #[test]
 pub fn hot_leg_validation_set_c8(){
 
@@ -362,8 +362,8 @@ pub fn hot_leg_validation_set_c8(){
 }
 
 
-// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
-// C-9,0.03547,119.96879,116.05003,79.02407,76.54479,
+/// dataset number,pri loop mass flowrate (kg/s),Heater outlet (DegC),DHX shell top (DegC),DHX shell bottom (DegC),Heater inlet (DegC),
+/// C-9,0.03547,119.96879,116.05003,79.02407,76.54479,
 #[test]
 pub fn hot_leg_validation_set_c9(){
 
